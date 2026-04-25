77b502df2df37ec128701a37adbdc9a06ea23446
alizodasabrina <alizodasabrina@gmail.com>
2026-04-25 22:49:36 +0500

    feat(onboarding): add two-screen onboarding with UserDefaults first-launch detection

--- iPods/iPods/Onboarding/OnboardingVC.swift
+++ iPods/iPods/Onboarding/OnboardingVC.swift
@@ -1,3 +1,31 @@
 import UIKit
 
-final class OnboardingVC {}
+final class OnboardingVC: UIPageViewController {
+
+    static let hasSeenOnboardingKey = "hasSeenOnboarding"
+
+    private let firstVC = OnboardingFirstVC()
+    private let secondVC = OnboardingSecondVC()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firstVC.onGetStarted = { [weak self] in
+            guard let self else { return }
+            setViewControllers([secondVC], direction: .forward, animated: true)
+        }
+
+        secondVC.onContinue = { [weak self] in
+            self?.finish()
+        }
+
+        setViewControllers([firstVC], direction: .forward, animated: false)
+    }
+
+    private func finish() {
+        UserDefaults.standard.set(true, forKey: OnboardingVC.hasSeenOnboardingKey)
+
+        guard let scene = view.window?.windowScene,
+              let delegate = scene.delegate as? SceneDelegate else { return }
+        delegate.showHome()
+    }
+}
