a66932f5ef9acfb42c10d3f407183ba1b04a1b56
michaelbolgar <mikhail.bolgar@uni-oldenburg.de>
2026-05-06 02:45:08 +0200

    implemented details screen

---
+++ iPods/iPods/Home/Models/PodcastMapper.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+struct PodcastMapper {
+
+    static func toFull(_ podcast: Podcast) -> PodcastFull {
+        return PodcastFull(
+            id: podcast.id,
+            title: podcast.title,
+            author: podcast.author,
+            genre: podcast.genre,
+            rating: podcast.rating,
+            episodeCount: podcast.episodeCount,
+            description: podcast.description,
+            progress: 0.0,
+            artworkURL: nil,
+            feedID: nil
+        )
+    }
+}
